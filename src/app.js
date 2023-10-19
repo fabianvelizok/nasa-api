b91034e785c38293e8e79d345d742ef26121e949
Fabian Veliz <velizfabianhoracio@gmail.com>
2023-10-19 19:03:25 -0300

    Configure cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 
 const planetsRouter = require('./routes/planets/planets.router')
+const { CLIENT_URL } = require('./constants')
 
 const app = express()
 
 app.use(express.json())
-
+app.use(cors({ origin: CLIENT_URL }))
 app.use(planetsRouter)
 
 module.exports = app
