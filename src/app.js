e659f84084094b32dcf41f43173fecbd710f8948
Fabian Veliz <velizfabianhoracio@gmail.com>
2023-10-21 12:23:56 -0300

    Log requests using morgan

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const path = require('node:path')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planets.router')
 const { CLIENT_URL } = require('./constants')
 
 const app = express()
 
-app.use(express.json())
 app.use(cors({ origin: CLIENT_URL }))
+app.use(morgan('combined'))
+app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req, res) => {
