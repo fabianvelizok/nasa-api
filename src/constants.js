90fd71bf19ffa92409363a567fba15e199fe0f00
Fabian Veliz <velizfabianhoracio@gmail.com>
2023-10-29 13:35:08 -0300

    Install mongoose and connect to db

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,18 @@
+const process = require('node:process')
+
 const CLIENT_URL = 'http://localhost:3000'
 
+const PORT = process.env.PORT || 8000
+
+const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`
+
+if (!process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_URL) {
+  console.error('Required environment variables were not provided!')
+  process.exit(1)
+}
+
 module.exports = {
-    CLIENT_URL
+  CLIENT_URL,
+  PORT,
+  DB_URL
 }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 const http = require('node:http')
+const mongoose = require('mongoose');
 
+const { PORT, DB_URL } = require('./constants')
 const app = require('./app.js')
 const { loadPlanets } = require('./models/planets.model')
 
-const PORT = process.env.PORT || 8000
+mongoose.connection.on('open', () => {
+  console.log('Connected to db')
+})
+
+mongoose.connection.on('error', err => {
+  console.error('Error after initial connection. ', err)
+})
 
 async function startServer() {
+  try {
+    await mongoose.connect(DB_URL);
+  } catch (e) {
+    console.error('Error on initial connection to db. ', e)
+  }
+
   await loadPlanets()
 
   const server = http.createServer(app)
