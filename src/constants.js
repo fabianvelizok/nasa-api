079ad273952c884dca8067ca04971048bf8f6c00
Fabian Veliz <velizfabianhoracio@gmail.com>
2023-11-07 17:57:49 -0300

    Update env vars

--- src/constants.js
+++ src/constants.js
@@ -4,10 +4,9 @@ const CLIENT_URL = 'http://localhost:3000'
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4'
 
 const PORT = process.env.PORT || 8000
+const DB_URL = process.env.DB_URL
 
-const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`
-
-if (!process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_URL) {
+if (!DB_URL) {
   console.error('Required env variables were not provided. Set up the .env file.')
   process.exit(1)
 }
