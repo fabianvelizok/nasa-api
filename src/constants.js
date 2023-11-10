c2a1a04690c781e01c46df4c4967d20b1b72cc1e
Fabian Veliz <velizfabianhoracio@gmail.com>
2023-11-10 13:32:51 -0300

    Adjust port

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ const process = require('node:process')
 const CLIENT_URL = 'http://localhost:3000'
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4'
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 4000
 const DB_URL = process.env.DB_URL
 
 if (!DB_URL) {
